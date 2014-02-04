04fc31391554beb4831dd42b47a9974fbb2774ff
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 15:17:42 -0800

    Added random number generation

--- guess.py
+++ guess.py
@@ -1,7 +1,14 @@
+import random
+
 print "Hey there, welcome to our guessing game!"
 print "What's your name?"
 name = raw_input("> ")
 
+computer_number = random.randint(0, 100)
+
 # ask player to choose a random number between 1 and 100
-print "%d, I'm thinking of a number between 1 and 100. Try to guess my number." % name
+print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
+
+# while True:
+#     user_guess = int(raw_input("> "))
 
