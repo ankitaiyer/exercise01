2de9ccb7784d0579b6f71d62d13ed3187c73cc07
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 15:09:51 -0800

    Prompting user to guess number

--- guess.py
+++ guess.py
@@ -1,3 +1,7 @@
 print "Hey there, welcome to our guessing game!"
 print "What's your name?"
 name = raw_input("> ")
+
+# ask player to choose a random number between 1 and 100
+print "%d, I'm thinking of a number between 1 and 100. Try to guess my number." % name
+
