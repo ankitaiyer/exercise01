e962a14029809b099a199e105c2135151d828a5d
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 16:07:16 -0800

    Nested inside loop to ask player if they want to play again or not

--- guess.py
+++ guess.py
@@ -1,49 +1,60 @@
 # need random module to generate random integer
 import random
+from sys import exit
 
 # greet user and ask for name
 print "Hey there, welcome to our guessing game!"
 print "What's your name?"
 name = raw_input("> ")
 
-# generate random integer between 1 and 100
-computer_number = random.randint(1, 100)
-
-# ask player to choose a random number between 1 and 100
-print "%s, I'm thinking of a whole number between 1 and 100." % name,
-print "Try to guess my number."
-
-# initialize attempt counter
-count = 0
-
-# main loop: user guesses
 while True:
-    # increment the attempt counter
-    count += 1
+    # generate random integer between 1 and 100
+    computer_number = random.randint(1, 100)
 
-    # get user guess
-    user_input = raw_input("> ")
+    # ask player to choose a random number between 1 and 100
+    print "%s, I'm thinking of a whole number between 1 and 100." % name,
+    print "Try to guess my number."
 
-    # check to see if user guess is a number; if not, ask to guess again
-    if user_input.isdigit() == False:
-        print "Sorry, that doesn't seem to be a whole number."
-        print "Please enter a number between 1 and 100."
+    # initialize attempt counter
+    count = 0
 
-    else:
-        
-        # convert user guess to an integer
-        user_guess = int(user_input)
+    # main loop: user guesses
+    while True:
+        # increment the attempt counter
+        count += 1
 
-        # hints if user guess is incorrect and asks for new guess
-        if user_guess < 1 or user_guess > 100:
-            print "Sorry, that doesn't seem to be between 1 and 100.",
+        # get user guess
+        user_input = raw_input("> ")
+
+        # check to see if user guess is a number; if not, ask to guess again
+        if user_input.isdigit() == False:
+            print "Sorry, that doesn't seem to be a whole number."
             print "Please enter a number between 1 and 100."
-        elif user_guess > computer_number:
-            print "Your guess is too high, try again."
-        elif user_guess < computer_number:
-            print "Your guess is too low, try again."
 
-        # for correct guess, congratulate and end game
         else:
-            print "Congratulations, %s! You found my number, %d, in %d attempts!" % (name, computer_number, count)
-            break
+            
+            # convert user guess to an integer
+            user_guess = int(user_input)
+
+            # hints if user guess is incorrect and asks for new guess
+            if user_guess < 1 or user_guess > 100:
+                print "Sorry, that doesn't seem to be between 1 and 100.",
+                print "Please enter a number between 1 and 100."
+            elif user_guess > computer_number:
+                print "Your guess is too high, try again."
+            elif user_guess < computer_number:
+                print "Your guess is too low, try again."
+
+            # for correct guess, congratulate and end game
+            else:
+                print "Congratulations, %s! You found my number, %d, in %d attempts!" % (name, computer_number, count)
+                print "Would you like to play again? Please type Y for yes or N for no."
+                play_again = raw_input("> ")
+
+                if play_again == "Y":
+                    break
+                elif play_again == "N":
+                    print "Okay, thanks for playing!"
+                    exit(0)
+                else:
+                    print "Sorry, please type Y or N."
